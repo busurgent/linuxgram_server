bad6e1eb2a6d260d807f4816d455a01a8f903cec
busurgent <bus-ilyukha@mail.ru>
2025-02-22 23:40:10 +0300

    shit for pintisuka

--- server.cpp
+++ server.cpp
@@ -10,93 +10,92 @@ class session: public std::enable_shared_from_this<session> {
         socket(std::move(socket)) {}
 
     void read_from_socket() {
-        boost::asio::async_read_until(
-            socket,
-            streambuf,
-            '\n',
-            [self = shared_from_this()](
-                boost::system::error_code error,
-                std::size_t bytes_transferred) {
-                    self->data = std::string(boost::asio::buffers_begin((self->streambuf).data()), boost::asio::buffers_end((self->streambuf).data()));
-                    std::cout << "read" << std::endl;
+    boost::asio::async_read_until(
+        socket,
+        streambuf,
+        '\n',
+        [self = shared_from_this()](
+            boost::system::error_code error,
+            std::size_t bytes_transferred) {
+                if (!error) {
+                    self->data = std::string(boost::asio::buffers_begin(self->streambuf.data()), 
+                                             boost::asio::buffers_end(self->streambuf.data()));
                     self->query_analyzer();
-                    // self->streambuf.consume(self->streambuf.size());
-                    // std::cout << data;
-
+                    self->streambuf.consume(bytes_transferred);
                     self->write_to_socket();
+                } else {
+                    std::cerr << "Ошибка чтения: " << error.message() << std::endl;
+                    // Можно закрыть соединение или предпринять другие действия
+                }
+        });
+}
 
-
-            });
-    }
-
-    void query_analyzer()
-    {
+    void query_analyzer() {
+    try {
         std::cout << data << std::endl;
-        nlohmann::json j = nlohmann::json::parse(data);
-        json.clear();
-
+        auto j = nlohmann::json::parse(data);
         const char* conninfo = "dbname=linuxgram user=ilya hostaddr=127.0.0.1 port=5432 password=qwerty";
         PGconn* conn = PQconnectdb(conninfo);
         if (PQstatus(conn) != CONNECTION_OK) {
             std::cerr << "Ошибка подключения к базе данных: " << PQerrorMessage(conn) << std::endl;
             PQfinish(conn);
-        }
-        // if (j["type"] == "register") {
-            char format[1024];
-            std::cout << "bd_query1" << std::endl;
-            std::string login = j["username"];
-            std::string password = j["password"];
-
-            std::cout << "bd_query2" << std::endl;
-            
-            snprintf(format, 1024, "INSERT INTO users (username, password, token) VALUES (\'%s\', \'%s\', \'%s\');", login.c_str(), password.c_str(), "88005553535");
-            PGresult* res = PQexec(conn, format);
-        // }
-
-        // else if (j["type"] == "login") {
-
-        // }
-
-        // else if (j["type"] == "search") {
-
-        // }
-
-        // else if (j["type"] == "select_user") {
-
-        // }
-
-        // else if (j["type"] == "message") {
-
-        // }
-
-        if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-            std::cerr << "Ошибка выполнения запроса: " << PQerrorMessage(conn) << std::endl;
-            PQclear(res);
-            PQfinish(conn);
             return;
         }
 
-        PQclear(res);
+        if (j["type"] == "register") {
+            std::string login = j["username"];
+            std::string password = j["password"];
+            const char* paramValues[2] = { login.c_str(), password.c_str() };
+
+            PGresult* res = PQexecParams(
+                conn,
+                "INSERT INTO users (username, password, token) VALUES ($1, $2, '88005553535')",
+                2,       // Количество параметров
+                nullptr, // Типы параметров (nullptr → определяются автоматически)
+                paramValues,
+                nullptr, // Длины параметров
+                nullptr, // Форматы параметров
+                0        // Формат результата (0 = текст)
+            );
+
+            if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+                std::cerr << "Ошибка выполнения запроса: " << PQerrorMessage(conn) << std::endl;
+            }
+
+            PQclear(res);
+        }
+        json.clear();
+        json["status"] = "OK";
+        json["user_id"] = 0;
+        std::ostream os(&streambuf1);
+        os << json.dump();
+
         PQfinish(conn);
-
-        return;
+    } catch (const std::exception& e) {
+        std::cerr << "Ошибка обработки JSON или SQL-запроса: " << e.what() << std::endl;
     }
+}
 
-    void write_to_socket()
-    {
-        boost::asio::async_write(
-            socket,
-            streambuf, 
-            [self = shared_from_this()](
-                boost::system::error_code error,
-                    std::size_t bytes_transferred){
-                        self->read_from_socket();
-                    });
-    }
+    void write_to_socket() {
+    boost::asio::async_write(
+        socket,
+        streambuf1,
+        [self = shared_from_this()](
+            boost::system::error_code error,
+            std::size_t bytes_transferred) {
+                if (!error) {
+                    self->read_from_socket();
+                } else {
+                    std::cerr << "Ошибка записи: " << error.message() << std::endl;
+                    // Закрыть соединение или обработать ошибку
+                }
+        });
+}
 
   private:
     boost::asio::ip::tcp::socket socket;
     boost::asio::streambuf streambuf;
+    boost::asio::streambuf streambuf1;
     std::string data;
     nlohmann::json json;
 };
